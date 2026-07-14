e75d0b35009ba3535570366db4dbe02bc41175da
karrarix <barzankarar@gmail.com>
2026-07-14 12:13:21 +0300

    its done the app is ready

--- remoteD.bat
+++ remoteD.bat
@@ -1,4 +1,47 @@
-winget install chocolatey.chocolatey
+@echo off
+
+if NOT exist  "C:\ProgramData\chocolatey\choco.exe" winget install chocolatey.chocolatey
 
 choco install rustdesk
-ecoho install complete now setup
+echo "install complete now setup"
+
+
+@echo off
+
+REM Assign the value random password to the password variable
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+set alfanum=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+set rustdesk_pw=
+for /L %%b in (1, 1, 12) do (
+    set /A rnd_num=!RANDOM! %% 62
+    for %%c in (!rnd_num!) do (
+        set rustdesk_pw=!rustdesk_pw!!alfanum:~%%c,1!
+    )
+)
+
+REM Get your config string from your Web portal and Fill Below
+set rustdesk_cfg="configstring"
+
+REM ############################### Please Do Not Edit Below This Line #########################################
+
+cd "C:\Program Files\RustDesk\"
+rustdesk.exe --install-service
+timeout /t 5
+
+for /f "delims=" %%i in ('rustdesk.exe --get-id ^| more') do set rustdesk_id=%%i
+
+rustdesk.exe --config %rustdesk_cfg%
+
+rustdesk.exe --password %rustdesk_pw%
+
+echo ...............................................
+REM Show the value of the ID Variable
+echo RustDesk ID: %rustdesk_id%
+
+REM Show the value of the Password Variable
+echo Password: %rustdesk_pw%
+echo ...............................................
+echo "now take a screen shot and give it to the tech guy"
+echo ...............................................
+
+pause
